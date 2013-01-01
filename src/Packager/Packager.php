ec06274f9d608e69984a179721d78a6cf230d1d1
Miles Johnson <mileswjohnson@gmail.com>
2013-01-01 15:37:37 -0800

    Updated docs Prepared tests

--- src/Packager/Packager.php
+++ src/Packager/Packager.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @copyright	Copyright 2006-2012, Miles Johnson - http://milesj.me
+ * @copyright	Copyright 2006-2013, Miles Johnson - http://milesj.me
  * @license		http://opensource.org/licenses/mit-license.php - Licensed under the MIT License
- * @link		http://milesj.me/code/cakephp/utility
+ * @link		http://milesj.me/code/php/packager
  */
 
 namespace Packager;
